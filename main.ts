3ec39c7b8a6163023f39123ac3b49b4f8bc9f142
Lacaca5024 <168985281+Lacaca5024@users.noreply.github.com>
2024-05-05 11:43:40 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,92 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(5000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(5000)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.clearScreen()
+})
